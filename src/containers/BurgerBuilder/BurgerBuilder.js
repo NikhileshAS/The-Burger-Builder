bbca99b99d1c92271de3866dbdd05b1da9e56eed
NikhileshAS <32016285+NikhileshAS@users.noreply.github.com>
2019-01-06 00:52:15 +0530

    Firebase with Axios

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const IngredientPrices = {
   salad: 5,
@@ -81,22 +82,31 @@ export default class BurgerBuilder extends Component {
   };
 
   confirmOrderHandler = () => {
-    alert("Your order is placed");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Nikhilesh",
+        address: {
+          street: "Brindhavan Avenue",
+          zip: "641687",
+          state: "Tamil Nadu",
+          country: "India"
+        },
+        email: "nikhi.ajay96@gmail.com"
+      },
+      deliveryMethod: "fastest"
+    };
+    axios
+      .post("/orders.json", order)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   };
 
-  refreshState = () => {
-    this.setState = {
-      ingredients: {
-        salad: 0,
-        bacon: 0,
-        cheese: 0,
-        meat: 0
-      },
-      totalPrice: 4,
-      purchasable: false,
-      purchasing: false
-    };
-  };
   render() {
     let ingredients = { ...this.state.ingredients };
     // let style = this.state.purchasing
